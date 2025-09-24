689424067d5198fda5833dc86c9dfdeb9a582002
Vishal496-beep <vishal.k08032004@gmail.com>
2025-09-24 10:55:57 +0000

    interview

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -2,11 +2,17 @@ import Chai from './chai.js'
 
 function App() {
   return (
-    <>
-     <Chai />
-   <h1>code snippet | vishal today i gained a little knowledge about react after this lecture i got motivated to create something</h1>
-   </>
-  );
-}
+      <>
+           <Chai />
+              <h1>code snippet | vishal today i gained a little knowledge about react after this lecture i got motivated to create something</h1>
+                 </>
+                   );
+                   }
 
-export default App;
+                   export default App;
+                   
+
+
+
+      //react-dom ===when we want to use in browser
+      //react native === when we wantt to use it in mobile
